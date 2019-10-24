51db7a09dbf4f0d3acbcd28ad811277df0841510
venkateshsundaram <contactvenkateshsundaram@gmail.com>
2019-10-24 14:51:42 +0530

    Initial commit

--- core/components/Theme/utils.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { themeDefault } from '../../constants/application';
-
-export var configTheme = function configTheme(theme) {
-  var themeConfigured = Object.assign({}, themeDefault, theme);
-  return themeConfigured;
-};
--- src/components/Theme/utils.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { themeDefault } from '../../constants/application';
-  
-  export const configTheme = (theme: Object) => {
-    let themeConfigured = {...themeDefault, ...theme};
-    return themeConfigured;
-  }
-  
-  
